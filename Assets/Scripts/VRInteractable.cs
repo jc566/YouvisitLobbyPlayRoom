0effeb87cddfd8c32f8713235f0641499d630060
Joseph Choi <jc566@njit.edu>
2017-06-17 15:24:46 -0400

    Initial Commit

---
+++ Assets/Scripts/VRInteractable.cs
@@ -0,0 +1,75 @@
+﻿/************************************************************
+ * This script was created by :                             *
+ * Joseph Choi / Jacob Maher / Shannon Hargrove / Lin Chen  *
+ ***********************************************************/
+using UnityEngine;
+
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class VRInteractable : MonoBehaviour
+{
+    //saves the original spin speed of an object with the spinScript attached
+    public float savedSpinSpeed;
+
+    //Material used when the object is not being gazed at
+    public Material inactiveMaterial;
+
+    //Material used when the object is being gazed at
+    public Material activeMaterial;
+
+    private Vector3 startingPosition;
+
+    //The amount to scale an object with SizeChangeScript.cs attached
+    private const float ScaleAmount = 0.07f;
+
+    void Start()
+    {
+        //startingPosition = transform.localPosition;
+        SetGazedAt(false);
+    }
+
+    public void SetGazedAt(bool gazedAt)
+    {
+        /*if (inactiveMaterial != null && gazedAtMaterial != null)
+        {
+            GetComponent<Renderer>().material = gazedAt ? gazedAtMaterial : inactiveMaterial;
+            return;
+        }
+        GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;*/
+
+        //Debug.Log(gazedAt);
+        printMessage();
+
+    }
+
+    public void Reset()
+    {
+        //transform.localPosition = startingPosition;
+    }
+
+   
+ 
+
+    //Print a Debug.log message
+    public void printMessage()
+    {
+        Debug.Log("This is Working");
+    }
+
+    public void Recenter()
+    {
+#if !UNITY_EDITOR
+    GvrCardboardHelpers.Recenter();
+#else
+        GvrEditorEmulator emulator = FindObjectOfType<GvrEditorEmulator>();
+        if (emulator == null)
+        {
+            return;
+        }
+        emulator.Recenter();
+#endif  // !UNITY_EDITOR
+    }
+
+
+}
